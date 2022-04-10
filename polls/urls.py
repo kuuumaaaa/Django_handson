72b13cb712bffe64fd6e5569e9ea15ad4860240b
kuuumaaaa <67354740+kuuumaaaa@users.noreply.github.com>
2022-04-10 11:35:40 +0900

    chapter 3

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+# app_name use for unique namespace
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    # First, the polls are read, 
+    # then the string matching <int:question_id> is read as the question_id.
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/resutlts/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,31 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
+# request is a HttpRequest object.
+# request is generated when user accesses th URL.
+# request has many methods.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
+    # render() takes two arguments:
+    # 1. template name
+    # 2. context dictionary
+    # reder() returns an HttpResponse object.
+    # render() is a shortcut for loader.get_template().render()
 
-def index(request, question_id):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    # get_object_or_404() is a shortcut for Question.objects.get()
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
